523980fa3be9f3eb44983e9ab3e689d2589402d4
nestreldalila@gmail.com <nestreldalila@gmail.com>
2023-02-21 10:08:15 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - print alphabet in lower case
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+	return (0);
+}
