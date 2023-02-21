e22e0f343543013defb34ba5ff4fc162170e0057
nestreldalila@gmail.com <nestreldalila@gmail.com>
2023-02-21 14:45:08 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	times_table(3);
-	_putchar('\n');
-	times_table(5);
-	_putchar('\n');
-	times_table(98);
-	_putchar('\n');
-	times_table(12);  
-	return (0);
+    print_times_table(3);
+    _putchar('\n');
+    print_times_table(5);
+    _putchar('\n');
+    print_times_table(98);
+    _putchar('\n');
+    print_times_table(12);  
+    return (0);
 }
