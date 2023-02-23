e38e7908b90355d6af2c2ee14ba5b3bc1afca018
nestreldalila@gmail.com <nestreldalila@gmail.com>
2023-02-23 11:01:35 -0800

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * more_numbers - printing more numbers
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			_putchar(y + '0');
+		}
+		_putchar('\n');
+	}
+}
