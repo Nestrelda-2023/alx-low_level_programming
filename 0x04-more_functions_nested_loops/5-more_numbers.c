9052aef33aef61b65e745c31dde9227794f83f76
nestreldalila@gmail.com <nestreldalila@gmail.com>
2023-02-23 11:11:15 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,11 +7,12 @@ void more_numbers(void)
 {
 	int x, y;
 
-	for (x = 0; x < 10; x++)
+	for (x = 1; x <= 10; x++)
 	{
-		for (y = 0; y < 15; y++)
+		for (y = 0; y <= 14; y++)
 		{
-			_putchar(y + '0');
+			if (y >= 10)
+			_putchar(y % 10 + '0');
 		}
 		_putchar('\n');
 	}
